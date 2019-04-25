b8fe8f5751dc2793686658773b0cc14e8ad747fa
User <artyom.hovakimyan01@gmail.com>
2019-04-25 14:30:32 +0400

    add migration

---
+++ ExpenseManager/ExpenseManager/Models/ExpenseDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext:DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+    }
+}
